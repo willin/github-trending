ba1af031fd6f946bf0b022654095b21f6eb7ddc9
Willin Wang <willin@willin.org>
2018-04-24 18:21:25 +0800

    add json type

--- src/index.js
+++ src/index.js
@@ -4,17 +4,22 @@ const path = require('path');
 const { writeFileSync } = require('fs');
 const { formatDate } = require('@dwing/common');
 
-
 axios.get('https://github.com/trending').then(({ data }) => {
   const $ = cheerio.load(data);
-  const date = formatDate('yyyy-MM-dd');
-  const md = [`# ${date}`, ''];
+  const arr = [];
   $('.repo-list li').each((i, elm) => {
     const lang = $(elm).find('[itemprop="programmingLanguage"]');
-    const langText = lang.length === 0 ? '' : ` 【${lang.text().replace(/\s/g, '')}】`;
-    const linkText = `[${$(elm).find('h3 a').text().replace(/\s/g, '')}](https://github.com${$(elm).find('h3 a').attr('href')})`;
-    md.push(`${i + 1}. ${linkText} ${langText}`);
+    const item = {
+      title: $(elm).find('h3 a').text().replace(/\s/g, ''),
+      url: `https://github.com${$(elm).find('h3 a').attr('href')}`
+    };
+    if (lang.length !== 0) {
+      item.lang = lang.text().replace(/\s/g, '');
+    }
+    arr.push(item);
   });
-  md.push('');
+  const date = formatDate('yyyy-MM-dd');
+  writeFileSync(path.resolve(__dirname, '../data/', `${date}.json`), JSON.stringify(arr, null, 2), 'utf-8');
+  const md = [`# ${date}`, '', ...arr.map(({ title, url, lang }, i) => `${i + 1}. [${title}](${url}) ${lang === '' ? '' : `【${lang}】`}`), ''];
   writeFileSync(path.resolve(__dirname, '../archives/', `${date}.md`), md.join('\n'), 'utf-8');
 });
